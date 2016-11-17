acc1c095f6fb94910404238a1e08547a4e3f66cd
Daniel Gallegos <dgalort@stanford.edu>
2016-11-16 22:50:47 -0800

    Board Created with clicking

--- dist/board.js
+++ dist/board.js
@@ -8,26 +8,109 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var HelloMessage = function (_React$Component) {
-  _inherits(HelloMessage, _React$Component);
+var classNames = function classNames(classes) {
+  var result = "";
+  for (var key in classes) {
+    if (classes[key]) {
+      result += key + " ";
+    }
+  }
+  return result;
+};
 
-  function HelloMessage() {
-    _classCallCheck(this, HelloMessage);
+var Board = function (_React$Component) {
+  _inherits(Board, _React$Component);
 
-    return _possibleConstructorReturn(this, (HelloMessage.__proto__ || Object.getPrototypeOf(HelloMessage)).apply(this, arguments));
+  function Board(props) {
+    _classCallCheck(this, Board);
+
+    var _this = _possibleConstructorReturn(this, (Board.__proto__ || Object.getPrototypeOf(Board)).call(this, props));
+
+    var board = [new Array(8), new Array(8), new Array(8), new Array(8), new Array(8), new Array(8), new Array(8), new Array(8)];
+    for (var i = 0; i < 8; i++) {
+      for (var j = 0; j < 8; j++) {
+        board[i][j] = 0;
+      }
+    }
+    board[0][0] = 1;
+    board[0][2] = 1;
+    board[0][4] = 1;
+    board[0][6] = 1;
+    board[1][0] = 2;
+    board[1][2] = 1;
+    board[1][4] = 1;
+    board[1][6] = 1;
+
+    board[6][0] = 3;
+    board[6][2] = 3;
+    board[6][4] = 4;
+    board[6][6] = 3;
+    board[7][0] = 3;
+    board[7][2] = 3;
+    board[7][4] = 3;
+    board[7][6] = 3;
+
+    _this.state = {
+      board: board,
+      selectedPiece: null
+    };
+
+    _this.onGridClick = _this.onGridClick.bind(_this);
+    return _this;
   }
 
-  _createClass(HelloMessage, [{
+  _createClass(Board, [{
+    key: "onGridClick",
+    value: function onGridClick(e) {
+      if (this.state.selectedPiece != e.currentTarget.id) {
+        this.setState({ selectedPiece: e.currentTarget.id });
+      } else {
+        this.setState({ selectedPiece: null });
+      }
+    }
+  }, {
     key: "render",
     value: function render() {
+      var board = new Array(8);
+      for (var i = 0; i < 8; i++) {
+        var row = new Array(8);
+        for (var j = 0; j < 8; j++) {
+          var id = i.toString() + j.toString();
+          var gridClasses = classNames({
+            "grid": true,
+            "off-grid": (i - j) % 2 == 0,
+            "selected-grid": this.state.selectedPiece == id
+          });
+          var piece = null;
+          if (this.state.board[i][j] != 0) {
+            var pieceClasses = classNames({
+              "piece": true,
+              "red-normal": this.state.board[i][j] == 1,
+              "red-king": this.state.board[i][j] == 2,
+              "black-normal": this.state.board[i][j] == 3,
+              "black-king": this.state.board[i][j] == 4
+            });
+            piece = React.createElement("div", { className: pieceClasses });
+          }
+          row[j] = React.createElement(
+            "div",
+            { className: gridClasses, id: id, onClick: this.onGridClick, key: j },
+            piece
+          );
+        }
+        board[i] = React.createElement(
+          "div",
+          { className: "row", key: i },
+          row
+        );
+      }
       return React.createElement(
         "div",
         null,
-        "Hello ",
-        this.props.name
+        board
       );
     }
   }]);
 
-  return HelloMessage;
+  return Board;
 }(React.Component);
--- src/board.jsx
+++ src/board.jsx
@@ -1,5 +1,95 @@
-class Board extends React.Component {
-  render() {
-    return <div>Hello {this.props.name}</div>;
+var classNames = function(classes) {
+  var result = "";
+  for (var key in classes) {
+    if (classes[key]) {
+      result += key + " "
+    }
   }
+  return result
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    var board = [new Array(8), new Array(8), new Array(8), new Array(8), new Array(8), new Array(8), new Array(8), new Array(8)];
+    for (var i = 0; i < 8; i++) {
+      for (var j = 0; j < 8; j++) {
+        board[i][j] = 0;
+      }
+    }
+    board[0][0] = 1;
+    board[0][2] = 1;
+    board[0][4] = 1;
+    board[0][6] = 1;
+    board[1][0] = 2;
+    board[1][2] = 1;
+    board[1][4] = 1;
+    board[1][6] = 1;
+
+    board[6][0] = 3;
+    board[6][2] = 3;
+    board[6][4] = 4;
+    board[6][6] = 3;
+    board[7][0] = 3;
+    board[7][2] = 3;
+    board[7][4] = 3;
+    board[7][6] = 3;
+
+    this.state = {
+      board: board,
+      selectedPiece: null,
+    }
+
+    this.onGridClick = this.onGridClick.bind(this);
+  }
+
+  onGridClick(e) {
+    if (this.state.selectedPiece != e.currentTarget.id) {
+      this.setState({selectedPiece: e.currentTarget.id});
+    } else {
+      this.setState({selectedPiece: null});
+    }
+  }
+
+  render() {
+    var board = new Array(8);
+    for (var i = 0; i < 8; i++) {
+      var row = new Array(8);
+      for (var j = 0; j < 8; j++) {
+        var id = i.toString() + j.toString();
+        var gridClasses = classNames({
+          "grid": true,
+          "off-grid": (i - j) % 2 == 0,
+          "selected-grid": this.state.selectedPiece == id,
+        });
+        var piece = null;
+        if (this.state.board[i][j] != 0) {
+          var pieceClasses = classNames({
+            "piece": true,
+            "red-normal": this.state.board[i][j] == 1,
+            "red-king": this.state.board[i][j] == 2,
+            "black-normal": this.state.board[i][j] == 3,
+            "black-king": this.state.board[i][j] == 4,
+          });
+          piece = <div className={pieceClasses} />;
+        }
+        row[j] = (
+          <div className={gridClasses} id={id} onClick={this.onGridClick} key={j}>
+            {piece}
+          </div>
+        );
+      }
+      board[i] = (
+        <div className="row" key={i}>
+          {row}
+        </div>
+      );
+    }
+    return (
+      <div>
+        {board}
+      </div>
+    );
+  }
+
 }
