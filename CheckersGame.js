ef45dc58562dd3a2b0e1daed8ed8317935748e87
Hugo Kitano <hkitano@stanford.edu>
2016-11-30 21:08:57 -0800

    alphabeta, plus fixes to bugs

--- CheckersGame.js
+++ CheckersGame.js
@@ -374,14 +374,15 @@ class checkersGame {
     }
 }
 
-class minimaxAgent {
+class minimaxAgent { 
     constructor() {
         this.depth = 4;
     }
 
     getAction(checkersState) {
-        get_best_action(checkersState, depth, max_depth, agent) { //eval function in state
+        var get_best_action = function (checkersState, depth, max_depth, agent, alpha_beta) { //eval function in state
             var legal_actions = checkersState.getLegalActions(agent);
+            shuffle(legal_actions);
             if (checkersState.isWin(agent) == true || checkersState.isLose(agent) == true || legal_actions.length == 0) {
                 return [null, checkersState.getScore()];
             }    
@@ -394,43 +395,59 @@ class minimaxAgent {
             if (agent == 0) { // player
                 for (i = 0; i < next_states.length; i++) { 
                     var next_state = next_states[i];
-                    var next_score = null;
-                    if (depth == max_depth) {
-                        next_score = checkersState.getScore()
-                    } else {
-                        next_score = get_best_action(next_state, depth, max_depth, 1)[1];
-                    } 
-                    if (i == 0) {
-                        best_action = 0;
-                        best_score = next_score;
-                    } else {
-                        if (next_score > best_score) {
-                            best_action = i;
+                    if ((alpha_beta[1] - alpha_beta[0]) > 0 ) {
+                        var next_score = null;
+                        if (depth == max_depth) {
+                            next_score = checkersState.getScore()
+                        } else {
+                            next_score = get_best_action(next_state, depth, max_depth, 1, alpha_beta)[1];
+                        } 
+                        if (i == 0) {
+                            best_action = 0;
                             best_score = next_score;
+                            if (best_score > alpha_beta[0]) {
+                                alpha_beta = [best_score, alpha_beta[1]]
+                            }
+                        } else {
+                            if (next_score > best_score) {
+                                best_action = i;
+                                best_score = next_score;
+                                if (best_score > alpha_beta[0]) {
+                                    alpha_beta = [best_score, alpha_beta[1]]
+                                }
+                            }
                         }
-                    }
+                    }  
                 }
-                return (legal_actions[best_action], best_score)
+                return [legal_actions[best_action], best_score]
             } else { //computer
                 for (i = 0; i < next_states.length; i++) { 
                     var next_state = next_states[i];
-                    var next_score = get_best_action(next_state, depth + 1, max_depth, 1)[1];
-                    if (i == 0) {
-                        best_action = 0;
-                        best_score = next_score;
-                    } else {
-                        if (next_score < best_score) {
-                            best_action = i;
+                    if ((alpha_beta[1] - alpha_beta[0]) > 0) {
+                        var next_score = get_best_action(next_state, depth + 1, max_depth, 1, alpha_beta)[1];
+                        if (i == 0) {
+                            best_action = 0;
                             best_score = next_score;
+                            if (best_score < alpha_beta[1]) {
+                                alpha_beta = [alpha_beta[0], best_score];
+                            }
+                        } else {
+                            if (next_score < best_score) {
+                                best_action = i;
+                                best_score = next_score;
+                                if (best_score < alpha_beta[1]) {
+                                    alpha_beta = [alpha_beta[0], best_score];
+                                }
+                            }
                         }
                     }
                 }
-                return (legal_actions[best_action], best_score)
+                return [legal_actions[best_action], best_score]
             }
-        }
-
-    var best_action = get_best_action(checkersState, 0, self.depth, 0)
-    return best_action[0]
+        } 
+    var alpha_beta = [0 - Infinity, Infinity];
+    var best_action = get_best_action(checkersState, 0, self.depth, 0, alpha_beta);
+    return best_action[0];
     }
 }
 
