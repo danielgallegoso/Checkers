4afc40ecc7c0a2e15a5b0109b114a181d0492022
Marco Monteiro <Marco@DN51u4pt.SUNet>
2016-12-03 16:30:53 -0800

    gnereate transcript

--- generateTranscript.js
+++ generateTranscript.js
@@ -16,13 +16,51 @@ function generateTranscript(){
     var moves = input.split(' ');
     var map = new Object();
     map['1'] = [1,0];
-    map['3'] = [3,0];
-    map['4'] = [5,0];
-    map['5'] = [7,0];
-    console.log(map);
+    map['2'] = [3,0];
+    map['3'] = [5,0];
+    map['4'] = [7,0];
+    map['5'] = [0,1];
+    map['6'] = [2,1];
+    map['7'] = [4,1];
+    map['8'] = [6,1];
 
+    map['9'] = [1,2];
+    map['10'] = [3,2];
+    map['11'] = [5,2];
+    map['12'] = [7,2];
+    map['13'] = [0,3];
+    map['14'] = [2,3];
+    map['15'] = [4,3];
+    map['16'] = [6,3];
 
+    map['17'] = [1,4];
+    map['18'] = [3,4];
+    map['19'] = [5,4];
+    map['20'] = [7,4];
+    map['21'] = [0,5];
+    map['22'] = [2,5];
+    map['23'] = [4,5];
+    map['24'] = [6,5];
 
+    map['25'] = [1,6];
+    map['26'] = [3,6];
+    map['27'] = [5,6];
+    map['28'] = [7,6];
+    map['29'] = [0,7];
+    map['30'] = [2,7];
+    map['31'] = [4,7];
+    map['32'] = [6,7];
+
+    console.log(moves);
+    test = moves
+
+    var game = new checkersGame();
+
+    for (var i=0; i <moves.length - 1; i+=3) {
+        agentZeroMove = moves[i+1];
+
+        agentOneMove = moves[i+2];
+    }
 
 
 }
