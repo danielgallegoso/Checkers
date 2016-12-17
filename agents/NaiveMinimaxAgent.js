2b47b1e77160d79e1c1508fbf901d4a7295fba91
Hugo Kitano <hkitano@stanford.edu>
2016-12-16 21:05:53 -0800

    e

--- agents/NaiveMinimaxAgent.js
+++ agents/NaiveMinimaxAgent.js
@@ -1,12 +1,17 @@
 class NaiveMinimaxAgent {
   constructor(agent) {
     var evalFunc = function(game) {
-      return game.getScore();
+        if (agent == 0) {
+            score = checkersState.getScore();
+        } else {
+            score = 0 - checkersState.getScore();
+        }
+      	return score;
     }
     this.minimax = new minimaxAgent(evalFunc, 4, agent);
   }
 
   getAction(game) {
-    return this.minimax.getAction(game);
+  	return this.minimax.getAction(game);
   }
 }
